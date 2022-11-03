9cb10739a1941755ca5a51a04477b7f6032beccd
Mark Vogt <markevogt@outlook.com>
2022-11-02 22:28:35 -0500

    IN-PROGRESS... COMPLETED base code to LISTEN for messages FROM IoT Hub and WRITE those messages BACK to IoT Hub NOTE - code MUST as a CONTAINER actually INSIDE an AZIoT DEVICE ! TRIED to build image but ERROR on step 4/5... SAVED | COMMITTED | PUSHED.

--- main.py
+++ main.py
@@ -17,34 +17,42 @@ from packaging.version import parse
 
 async def main():
     try:
+        # RETRIEVE the current system version as a String...
         str_currentPythonVersion = python_version()
+        # PARSE the current Python version into an actual Python Version OBJECT...
         currentPythonVersion = parse(str_currentPythonVersion)
+        # PARSE the minimalPythonVersion into an actual Python Version OBJECT...
         minimalPythonVersion = parse("3.5.3")
+        # CHECK to see if running a USEABLE version of Python; otherwise raise an exception... 
         if not currentPythonVersion >= minimalPythonVersion:
-        #if not currentPythonVersion >= "3.5.3":
-        # 2022 11 01 MV: this ORIGINAL code COULDN'T POSSIBLY WORK - it's complete BULLSHIT >:-(  ) !!! 
-        #if not sys.version >= "3.5.3":
             raise Exception( "The sample requires python 3.5.3+. Current version of Python: %s" % sys.version )
+
+        # DISPLAY a diagnostic message...    
         print ( "IoT Hub Client for Python" )
 
         # The client object is used to interact with your Azure IoT hub.
-        module_client = IoTHubModuleClient.create_from_edge_environment()
-
+        # 2022 11 02 MV: ALERT - this code can ONLY be executed from WTHIN AN AZURE IOT DEVICE !!!
+        str_device2_connectionstring = "HostName=iot-2022-VOGTLAND-demo-01.azure-devices.net;DeviceId=PANASONIC-TOUGHBOOK-MEV-02;SharedAccessKey=+RnUVBQkNf8rdENiiQni/bajfYmdUSPVQvakRLeHINA="
+        #module_client = IoTHubModuleClient.create_from_edge_environment()
+        module_client = IoTHubModuleClient.create_from_connection_string(str_device2_connectionstring)
         # connect the client.
         await module_client.connect()
 
-        # define behavior for receiving an input message on input1
+        # DEFINE behavior for receiving a C2D (Cloud-to-Device) input message on input1...
         async def input1_listener(module_client):
             while True:
+                # DISPLAY the incoming C2D data in the console...
                 input_message = await module_client.receive_message_on_input("input1")  # blocking call
-                print("the data in the message received on input1 was ")
+                print("input_message.data received on input1 = ")
                 print(input_message.data)
-                print("custom properties are")
+                print("input_message.custom properties received on input1 =")
                 print(input_message.custom_properties)
-                print("forwarding mesage to output1")
+
+                # COMPOSE & SEND BACK a "message recieved" message BACK to the IoT Hub...
+                print("ECHOING the received message BACK to the IoT Hub...")
                 await module_client.send_message_to_output(input_message, "output1")
 
-        # define behavior for halting the application
+        # DEFINE behavior for halting the application
         def stdin_listener():
             while True:
                 try:
@@ -55,16 +63,14 @@ async def main():
                 except:
                     time.sleep(10)
 
-        # Schedule task for C2D Listener
+        # SCHEDULE a task for a C2D 'Listener'...
         listeners = asyncio.gather(input1_listener(module_client))
-
         print ( "The sample is now waiting for messages. ")
 
-        # Run the stdin listener in the event loop
+        # LAUNCH the stdin listener in its own asynchronous (independent) event-listening loop...
         loop = asyncio.get_event_loop()
         user_finished = loop.run_in_executor(None, stdin_listener)
-
-        # Wait for user to indicate they are done listening for messages
+        # AWAIT the user to indicate they are done listening for messages because they pressed "Q" or "q"...
         await user_finished
 
         # Cancel listening
@@ -73,14 +79,11 @@ async def main():
         # Finally, disconnect
         await module_client.disconnect()
 
+    # HANDLE all exceptions raised through a common exception 'e'...
     except Exception as e:
-        print ( "Unexpected error %s " % e )
+        print ( "EXCEPTION thrown; e = %s " % e )
         raise
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
-
-    # If using Python 3.7 or above, you can use following code instead:
-    # asyncio.run(main())
+    # LAUNCH an asyncrhonous independent thread which continually runs main()...
+    asyncio.run(main())
